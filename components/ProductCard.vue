16924bfaec088987fa17b5376942db5f5e2bf7d9
bright.ikhide <bright.ikhide@e4email.net>
2021-03-15 08:00:51 +0100

    basefiles

---
+++ components/ProductCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <vs-card>
+    <template #title>
+      <h3>{{ product.product_name }}</h3>
+    </template>
+    <template #img>
+      <img
+        :src="
+          product.product_varieties.length > 0
+            ? product.product_varieties[0].images[0].location
+            : ''
+        "
+        :alt="product.product_name"
+      />
+    </template>
+    <template #text>
+      <p>
+        {{ product.product_description }}
+      </p>
+    </template>
+    <template #interactions>
+      <vs-button class="btn-chat" shadow primary>
+        <span class="span">
+          Add to cart
+        </span>
+      </vs-button>
+    </template>
+  </vs-card>
+</template>
+
+<script>
+export default {
+  props: {
+    product: {
+      type: Object
+    }
+  }
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <br />
+    <br />
+    <vs-row>
+      <vs-col
+        v-for="(product, index) in products"
+        :key="index"
+        vs-type="flex"
+        vs-justify="center"
+        vs-align="center"
+        w="3"
+      >
+        <ProductCard :product="product" />
+      </vs-col>
+    </vs-row>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  async mounted() {
+    const { data } = await axios.get(
+      "https://avois.herokuapp.com/api/v1/products"
+    );
+    this.products = data;
+    console.log(data);
+  },
+  data() {
+    return {
+      products: []
+    };
+  }
+};
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+}
+</style>
